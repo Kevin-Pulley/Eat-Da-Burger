447089a3bbb3c9b39dc3fe7e33a8315571d0c69f
Kevin Pulley <skippy1025@gmail.com>
2020-11-03 17:40:50 -0600

    styled the page and removed the delte button and created a new partial handlebars

--- models/burger.js
+++ models/burger.js
@@ -17,12 +17,8 @@ var burger = {
     orm.update("burger", objColVals, condition, function(res) {
       cb(res);
     });
-  },
-  delete: function(condition, cb) {
-    orm.delete("burger", condition, function(res) {
-      cb(res);
-    });
   }
+
 };
 
 // Export the database functions for the controller (catsController.js).
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -27,7 +27,7 @@ $(function() {
   
       var newDevour = {
         name: $("#ca").val().trim(),
-        devour: $("[name=devour]:checked").val().trim()
+        devour: 0
       };
   
       // Send the POST request.
@@ -43,18 +43,5 @@ $(function() {
       );
     });
   
-    $(".delete-burger").on("click", function(event) {
-      var id = $(this).data("id");
-  
-      // Send the DELETE request.
-      $.ajax("/api/burger/" + id, {
-        type: "DELETE"
-      }).then(
-        function() {
-          console.log("deleted burger", id);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
+
   });
